2d5e65c9205fd8db8b82eef7e4e9bdd8080baa00
SALIFOU Ouzéïrou <salifououzerou25@gmail.com>
2025-12-02 23:55:25 +0100

    views-index

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    product_object = Product.objects.all()
+    item_name = request.GET.get("item-name")
+    if item_name != "" and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    return render(request, 'shop/index.html', {'product_object': product_object})
